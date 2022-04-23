eea07dd7506ad6caf0637f4f52584bf409b41f8f
unknown <vladancupric@gmail.com>
2022-04-23 09:22:56 +0200

    New commits

--- feedback-app/src/components/FeedbackItem.js
+++ feedback-app/src/components/FeedbackItem.js
@@ -1,13 +1,20 @@
+import {FaTimes} from "react-icons/fa"
 import React, {useState} from 'react';
 import Card from './shared/Card';
 
 const FeedbackItem = ({item}) => {
 
- 
+ const handleClick =()=>{
+
+ }
 
     return (
     <Card reverse={false}>
         <div className="num-display">{item.rating}</div>
+        {/* <button  onClick={()=>{
+          console.log(item.id)
+        }} className="close"><FaTimes color="purple"/></button> */}
+             <button  onClick={handleClick} className="close"><FaTimes color="purple"/></button>
         <div className="text-display">{item.text}</div>
    
       </Card>
