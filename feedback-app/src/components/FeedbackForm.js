32619e8b177b6cf775103295b6c4dc408347cef2
unknown <vladancupric@gmail.com>
2022-04-26 15:13:09 +0200

    New commits

--- feedback-app/src/components/FeedbackForm.js
+++ feedback-app/src/components/FeedbackForm.js
@@ -2,8 +2,10 @@ import React from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 import { useState } from "react";
+import RatingSelected from "./RatingSelected";
 
 const FeedbackForm = () => {
+    const [rating, setRating] = useState(10)
   const [text, setText] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
@@ -21,11 +23,14 @@ const FeedbackForm = () => {
     }
     setText(e.target.value);
   };
+
+
   return (
     <Card>
       <form>
         <h2>How would you rate your service whit us?</h2>
-        {/* {} */}
+       <RatingSelected  select={(rating)=>
+           setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
