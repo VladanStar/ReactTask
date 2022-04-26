7d669ed8765f75786f7bdd02e7d1dce44c7194a2
unknown <vladancupric@gmail.com>
2022-04-26 09:28:38 +0200

    New commits

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item)=> item.id !== id));
