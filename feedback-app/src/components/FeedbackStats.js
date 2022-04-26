663149e7346683caaa66594fbb11c573f5d859cb
unknown <vladancupric@gmail.com>
2022-04-26 09:37:41 +0200

    New commits

--- feedback-app/src/components/FeedbackStats.js
+++ feedback-app/src/components/FeedbackStats.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const FeedbackStats = ({feedback}) => {
+
+    // Calculate ratings avg
+    let average = feedback.reduce((acc, curr)=>{
+        return acc + curr.rating
+    },0) /feedback.lenght;
+
+    console.log(average);
   return (
     <div className='feedback-stats'>
-        <h4>{feedback.lenght}</h4>
+        <h4>{feedback.lenght} Reviews</h4>
+        <h4>Average Rating {average}</h4>
     </div>
-    
+
   )
 }
 
