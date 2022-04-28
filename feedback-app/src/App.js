b72f63a07e028fd7448d5f3af9913fc403dce798
unknown <vladancupric@gmail.com>
2022-04-28 14:17:37 +0200

    New commits

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,6 +5,8 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4} from "uuid";
+import AboutPage from "./components/pages/AboutPage";
+import {BrowseRouter as Router, Route} from "react-router-dom"
 import "./App.css";
 
 function App() {
@@ -21,14 +23,16 @@ function App() {
     }
   };
   return (
-    <>
+    <Router>
       <Header bgColor="red" textColor="blue" text="Vladan Cupric" />
       <div className="container">
         <FeedbackForm  handleAdd ={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <AboutPage />
+        <Route path="/about">This is about Route</Route>
       </div>
-    </>
+    </Router>
   );
 }
 
