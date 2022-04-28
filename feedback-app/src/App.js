72865d282e7bffaf8e87ada7f9e66fb697468c01
unknown <vladancupric@gmail.com>
2022-04-28 14:44:16 +0200

    New commits

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -4,33 +4,42 @@ import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-import { v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import AboutPage from "./components/pages/AboutPage";
-import {BrowseRouter as Router, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
- 
-  const addFeedback = (newFeedback) =>{
-    newFeedback.id = uuidv4()
-    setFeedback([ newFeedback, ...feedback])
-  }
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item)=> item.id !== id));
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
   return (
     <Router>
       <Header bgColor="red" textColor="blue" text="Vladan Cupric" />
       <div className="container">
-        <FeedbackForm  handleAdd ={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-        <AboutPage />
-        <Route path="/about">This is about Route</Route>
+        <Routes>
+        <Route path="/" element={
+          <>
+           <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+          </>
+        }>
+         
+        </Route>
+        <Route path="/about" element={AboutPage}>
+          This is about Route
+        </Route>
+        </Routes>
       </div>
     </Router>
   );
