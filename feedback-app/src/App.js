3c3be22955126faa0750494e6ca99f58ba1e4217
unknown <vladancupric@gmail.com>
2022-04-28 15:09:47 +0200

    New commits

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -7,6 +7,7 @@ import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4 } from "uuid";
 import AboutPage from "./components/pages/AboutPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutIconLink from "./components/AboutIconLink";
 import "./App.css";
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
           This is about Route
         </Route>
         </Routes>
+        <AboutIconLink />
       </div>
     </Router>
   );
